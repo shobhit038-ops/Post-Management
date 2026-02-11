55e5e86a3bbdfdfaeb8ba969d36871d5eae4604a
shobhit038-ops <shobhitverma038@gmail.com>
2026-02-11 23:07:17 +0530

    Add files via upload

---
+++ blog-management/src/main/java/com/example/blog_management/Service/EmailService.java
@@ -0,0 +1,25 @@
+package com.example.blog_management.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    @Autowired
+    private JavaMailSender javaMailSender;
+    @Async
+    public void sendEmail(String to,String subject,String body){
+        try{
+            SimpleMailMessage mail = new SimpleMailMessage();
+            mail.setTo(to);
+            mail.setSubject(subject);
+            mail.setText(body);
+            javaMailSender.send(mail);
+        }catch (Exception e){
+            System.out.println("Exception while sendEmail "+ e);
+        }
+    }
+}
