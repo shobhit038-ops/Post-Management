cdddd13652cf7ab24e0f405ef0dfc317bfd75a8b
shobhit038-ops <shobhitverma038@gmail.com>
2026-03-21 22:41:35 +0530

    Update securityconfig.java

--- blog-management/src/main/java/com/example/blog_management/config/securityconfig.java
+++ blog-management/src/main/java/com/example/blog_management/config/securityconfig.java
@@ -13,9 +13,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.*;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import java.util.*;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 
 @Configuration
 @EnableWebSecurity
